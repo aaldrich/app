826e3113d86edbdc269bb3068583ac5be57ea798
Adam Aldrich <porsche4me1999@gmail.com>
2011-09-20 15:23:27 -0500

    Pushing new changes

--- source/nothinbutdotnetstore/web/core/CommandRegistry.cs
+++ source/nothinbutdotnetstore/web/core/CommandRegistry.cs
@@ -1,10 +1,20 @@
-﻿namespace nothinbutdotnetstore.web.core
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace nothinbutdotnetstore.web.core
 {
     public class CommandRegistry : IFindCommands
     {
+        readonly IEnumerable<IProcessOneRequest> all_the_commands;
+
+        public CommandRegistry(IEnumerable<IProcessOneRequest> all_the_commands)
+        {
+            this.all_the_commands = all_the_commands;
+        }
+
         public IProcessOneRequest get_the_command_that_can_process(IContainRequestInformation request)
         {
-            throw new System.NotImplementedException();
+            return all_the_commands.First(x => x.can_process(request));
         }
     }
 }
